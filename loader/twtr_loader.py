a70aa54a008350f41f5c3f1f8535e390b6d06495
szymsta <stanislawszymanski89@gmail.com>
2025-03-11 20:30:30 +0100

    Add methods to load data for Grammys, and Financial categories in TwtrLoader class

--- loader/twtr_loader.py
+++ loader/twtr_loader.py
@@ -1,4 +1,5 @@
 from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql.functions import lit
 
 
 class TwtrLoader:
@@ -16,4 +17,20 @@ class TwtrLoader:
                 .options(header=True, inferSchema=True, delimiter=",")
                 .load("covid19_tweets.csv")
                 .withColumn("category", lit(TwtrLoader.COVID_LABEL))
+                .na.drop())
+    
+
+    def load_grammys(self) -> DataFrame:
+        return (self.spark.read.format("csv")
+                .options(header=True, inferSchema=True, delimiter=",")
+                .load("GRAMMYs_tweets.csv")
+                .withColumn("category", lit(TwtrLoader.GRAMMYS_LABEL))
+                .na.drop())
+
+
+    def load_financial(self) -> DataFrame:
+        return (self.spark.read.format("csv")
+                .options(header=True, inferSchema=True, delimiter=",")
+                .load("financial.csv")
+                .withColumn("category", lit(TwtrLoader.FINANCE_LABEL))
                 .na.drop())

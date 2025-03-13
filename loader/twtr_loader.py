698d97349a8615edd48ab31e3a25e8c1f0699e97
szymsta <stanislawszymanski89@gmail.com>
2025-03-13 11:09:12 +0100

    Refactor TwtrLoader class to merge functions for loading datasets and add dataset union function

--- loader/twtr_loader.py
+++ loader/twtr_loader.py
@@ -1,36 +1,30 @@
 from pyspark.sql import SparkSession, DataFrame
 from pyspark.sql.functions import lit
+from functools import reduce
 
 
 class TwtrLoader:
 
-    COVID_LABEL = "covid"
-    GRAMMYS_LABEL = "grammys"
-    FINANCE_LABEL = "finance"
+    LABELS = {
+        "covid19_tweets.csv" = "covid",
+        "GRAMMYs_tweets.csv" = "grammys",
+        "financial.csv" = "finance"
+    }
+
 
     def __init__(self, spark_session: SparkSession):
         self.spark_session = spark_session
     
 
-    def load_covid(self) -> DataFrame:
+    def load_datasets(self, file_name: str) -> DataFrame:
+        label = self.LABELS.get(filename, "unknown")
         return (self.spark.read.format("csv")
                 .options(header=True, inferSchema=True, delimiter=",")
-                .load("covid19_tweets.csv")
-                .withColumn("category", lit(TwtrLoader.COVID_LABEL))
+                .load(filename)
+                .withColumn("category", lit(label))
                 .na.drop())
     
 
-    def load_grammys(self) -> DataFrame:
-        return (self.spark.read.format("csv")
-                .options(header=True, inferSchema=True, delimiter=",")
-                .load("GRAMMYs_tweets.csv")
-                .withColumn("category", lit(TwtrLoader.GRAMMYS_LABEL))
-                .na.drop())
-
-
-    def load_financial(self) -> DataFrame:
-        return (self.spark.read.format("csv")
-                .options(header=True, inferSchema=True, delimiter=",")
-                .load("financial.csv")
-                .withColumn("category", lit(TwtrLoader.FINANCE_LABEL))
-                .na.drop())
+    def union_datasets(self) -> DataFrame:
+        dfs = [self.load_datasets(file) for file in self.LABELS.keys()]
+        return reduce(DataFrame.unionByName, dfs)

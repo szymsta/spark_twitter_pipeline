977adb6da95fa8531ecda421c8603e0d38990b5c
szymsta <stanislawszymanski89@gmail.com>
2025-03-14 13:03:29 +0100

    Add null and empty string checks to search_keywords function.

--- analyser/twtr_searcher.py
+++ analyser/twtr_searcher.py
@@ -9,7 +9,7 @@ class TwtrSearcher:
     USER_LOCATION = "user_location"
 
     def __init__(self, spark_session: SparkSession):
-        self,spark_session: spark_session
+        self.spark_session: spark_session
     
 
     def search_keyword(self, key_word: str, df: DataFrame) -> DataFrame:
@@ -17,5 +17,10 @@ class TwtrSearcher:
     
 
     def search_keywords(self, key_words: list, df: DataFrame) -> DataFrame:
-        search_criteria = [lower(col(self.TEXT)).contains(key_word.lower()) for key_word in key_words]
+    
+        search_criteria = [
+            (col(self.TEXT).isNotNull() & (col(self.TEXT) != ""))
+            & lower(col(self.TEXT)).contains(key_word.lower()) for key_word in key_words
+            ]
+            
         return df.filter(reduce(lambda x, y: x | y, search_criteria ))

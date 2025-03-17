4bd63bb05087afcd8d286fbffabe106ca2c44464
szymsta <stanislawszymanski89@gmail.com>
2025-03-17 11:18:17 +0100

    Adds docstrings to file twtr_cleaner.py.

--- cleaner/twtr_cleaner.py
+++ cleaner/twtr_cleaner.py
@@ -4,12 +4,30 @@ from pyspark.sql.types import LongType, DateType
 
 
 class TwtrCleaner:
+    """
+    The TwtrCleaner class is responsible for cleaning and transforming the raw tweet data.
+    It handles operations such as cleaning and formatting columns in the dataset.
+    """
 
     def __init__(self, spark_session : SparkSession):
+        """
+        Initializes the TwtrCleaner object.
+
+        :param spark_session: An instance of SparkSession to handle data operations.
+        """
         self.spark_session = spark_session
 
 
     def clean_dataset(self, df: DataFrame) -> DataFrame:
+        """
+        Cleans and transforms the input dataset by:
+        - Removing unwanted characters from the "hashtags" column.
+        - Casting the "date" and "user_created" columns to DateType.
+        - Casting the "user_followers", "user_friends", and "user_favourites" columns to LongType.
+
+        :param df: The DataFrame containing the raw tweet data.
+        :return: A cleaned DataFrame with properly formatted columns.
+        """
         return (df.withColumn("hashtags", split(regexp_replace(col("hashtags"), r'["\'\[\] _-]', ""), ","))
                 .withColumn("date", col("date").cast(DateType()))
                 .withColumn("user_created", col("user_created").cast(DateType()))
